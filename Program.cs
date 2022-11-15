bd0d2160e5c4e202100032ff654445b8f41cb2fc
igoss11 <igoss11@gmail.com>
2022-11-15 06:19:15 +0300

    add all files

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arrayIn = new string[3] { "Hello", "2", "world" };
+string[] arrayOut = new string[arrayIn.Length];
+
+void CutArray(string[] arrayIn, string[] arrayOut)
+{
+    int count = 0;
+    for (int i = 0; i < arrayIn.Length; i++)
+    {
+        if (arrayIn[i].Length <= 3)
+        {
+            arrayOut[count] = arrayIn[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+}
+
+CutArray(arrayIn, arrayOut);
+PrintArray(arrayOut);
